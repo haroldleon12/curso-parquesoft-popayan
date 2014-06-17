d9e53f7cfecf81a2b2187df903fd7409bf320900
your name <ruby@ruby-VirtualBox.(none)>
2014-06-17 08:17:35 -0500

    creacion primera aplicacion rai

---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ActiveRecord::Base
+  attr_accessible :Last_name, :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parquesoft::Application.config.secret_token = 'ce4b4da20800be7747af5855fa5b9e67b883c52e81c4be9a752a07ba1d6673f9234e8666707d73f3852746690bb3c0435d5cd622889e1d1aa0aab95c15a03b44'
